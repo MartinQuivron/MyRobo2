b4fb23c3b41750b78ac1c69a6f480c888a7433b6
Adel <adeleuta@gmail.com>
2024-05-21 08:45:46 +0200

    Integration model

--- https_server.py
+++ https_server.py
@@ -1,7 +1,13 @@
 import http.server
 import ssl
+import socket
 
-server_address = ('192.168.100.207', 4443)
+def get_ip_address():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+server_address = (get_ip_address(), 4443)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./cert/cert.pem', keyfile='./cert/key.pem', server_side=True)
 
